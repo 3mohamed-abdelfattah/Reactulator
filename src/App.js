ba2ea9d8b966ba72576c262d404a67690a9b8c05
MohamedMohamed <m01126165016@icloud.com>
2024-07-27 15:08:22 +0300

    Done!!

--- src/App.js
+++ src/App.js
@@ -1,63 +1,68 @@
 import {
   useState,
   useRef
-} from "react"; 
+} from "react";
 import "./App.css";
 
-function App() { 
-  const inputRef = useRef(null); 
-  const resultRef = useRef(null); 
-  const [result, setResult] = useState(0); 
- 
-  function plus(e) { 
-    e.preventDefault(); 
-    setResult((result) => result + Number(inputRef.current.value)); 
-  }; 
- 
-  function minus(e) { 
-  	// Add the code for the minus function 
+function App() {
+  const inputRef = useRef(null);
+  const resultRef = useRef(null);
+  const [result, setResult] = useState(0);
+
+  function plus(e) {
+    e.preventDefault();
+    setResult((result) => result + Number(inputRef.current.value));
   };
- 
-  function times(e) { 
-    // Add the code for the plus function 
-  }; 
- 
-  function divide(e) { 
-    // Add the code for the divide function 
+
+  function minus(e) {
+    e.preventDefault();
+    setResult((result) => result - Number(inputRef.current.value))
   };
- 
-  function resetInput(e) { 
-    // Add the code for the resetInput function 
-  }; 
- 
-  function resetResult(e) { 
-  	// Add the code for the resetResult function 
-  }; 
- 
-  return ( 
-    <div className="App"> 
-      <div> 
-        <h1>Simplest Working Calculator</h1> 
-      </div> 
-      <form> 
-        <p ref={resultRef}> 
-          {/* add the value of the current total */} 
-        </p> 
+
+  function times(e) {
+    e.preventDefault();
+    setResult((result) => result * Number(inputRef.current.value))
+  };
+
+  function divide(e) {
+    e.preventDefault();
+    setResult((result) => result / Number(inputRef.current.value))
+  };
+
+  function resetInput(e) {
+    e.preventDefault();
+    Number(inputRef.current.value = 0)
+  };
+
+  function resetResult(e) {
+    e.preventDefault();
+    setResult((result) => result = 0)
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h1>Simplest Working Calculator</h1>
+      </div>
+      <form>
+        <p ref={resultRef}>
+          {result}
+        </p>
         <input
-          pattern="[0-9]" 
-          ref={inputRef} 
-          type="number" 
-          placeholder="Type a number" 
-        /> 
-        <button onClick={plus}>add</button> 
-        {/* Add the subtract button */} 
-        {/* Add the multiply button */} 
-        {/* Add the divide button */} 
-        {/* Add the resetInput button */} 
-        {/* Add the resetResult button */} 
-      </form> 
-    </div> 
-  ); 
-} 
- 
-export default App; 
+          pattern="[0-9]"
+          ref={inputRef}
+          type="number"
+          placeholder="Type a number"
+        />
+        <button onClick={plus}>add</button>
+        <button onClick={minus}>subtract</button>
+        <button onClick={times}>multiply</button>
+        <button onClick={divide}>divide</button>
+        <button onClick={resetInput}>resetInput</button>
+        <button onClick={resetResult}>resetResult</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
